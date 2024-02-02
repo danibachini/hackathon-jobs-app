4e3d230c817c62d4c1d6009636df0f6e035e5c15
DANI BACHINI <dani_costa90@yahoo.com.br>
2024-02-02 09:41:22 +0100

    db_connection.test file working

--- back_hackathon/Test/integration_tests/db_connection.test.js
+++ back_hackathon/Test/integration_tests/db_connection.test.js
@@ -1,3 +1,9 @@
-import { prisma } from '../../mysql_client';
+import { prisma } from '../../app/mysql_client';
 import { describe, expect, it } from 'vitest';
 
+describe('Db connection', () => {
+    it('should establish connection to the db', () => {
+        const users = prisma.user.findMany();
+        expect(users).not.toBe(null);
+    });
+});
