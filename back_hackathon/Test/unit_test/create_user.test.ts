337157aec01d70e4f50bc121e5ae3f845b974e8e
DANI BACHINI <dani_costa90@yahoo.com.br>
2024-02-15 18:03:54 +0100

    integration test db_connection working for connection, sign up and sign in

--- back_hackathon/Test/unit_test/create_user.test.ts
+++ back_hackathon/Test/unit_test/create_user.test.ts
@@ -1,4 +1,4 @@
-import {describe, expect, it, vi} from "vitest"
+import { describe, expect, it, vi } from "vitest"
 import prisma from "../../libs/__mocks__/prisma"
 import { createUser } from "../../app/Services/userService"
 import { Role } from "@prisma/client"
